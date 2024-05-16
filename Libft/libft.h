eb21402cca7ecdf80aac93566a3310d590dc5960
Maria Puyo <mariapuyo@MacBook-Air-de-Maria.local>
2024-05-16 20:31:53 +0200

    Subida ft_striteri

--- Libft/libft.h
+++ Libft/libft.h
@@ -53,6 +53,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 char	*ft_strrchr(const char *s, int c);
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
 char	*ft_strjoin(char const *s1, char const *s2);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
