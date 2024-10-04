574dc4a8bdf8d0aa8c3521aff7025d89272903f5
mariapuyo70 <166420887+mariapuyo70@users.noreply.github.com>
2024-10-04 11:39:38 +0200

    Subida get_netx_line.h

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maria <mpuyo-ro@student.42malaga.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 11:28:40 by maria             #+#    #+#             */
+/*   Updated: 2024/10/04 11:29:44 by maria            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
