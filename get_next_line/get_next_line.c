b75f628d8fe3a589ede494234709d9682d23d6df
Maria Puyo Roldan <mpuyo-ro@c2r1s2.42malaga.com>
2024-10-25 12:47:35 +0200

    Arreglo read_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,27 +6,18 @@
 /*   By: mpuyo-ro <mpuyo-ro@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:34:16 by maria             #+#    #+#             */
-/*   Updated: 2024/10/17 17:58:33 by mpuyo-ro         ###   ########.fr       */
+/*   Updated: 2024/10/25 09:54:49 by mpuyo-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	int		bytes_read;
-	int 	i;
-	char	*buffer;
-	char 	*result;
-
-	t = 0;
-
-	bytes_read = read(fd, buffer, BUFFER_SIZE)
-	if (bytes_read == -1)
-		return(1);
-}
 char	extract_line(buffer)
 {
+	char 	*result;
+	int 	i;
+	char	*buffer;
+
 	while (buffer[i] && buffer[i] != /n)
 		i++;
 	result = malloc(sizeof (char) * (i + 2))
@@ -35,9 +26,9 @@ char	extract_line(buffer)
 		result[i++] == buffer[i++];
 	result[i++] == '/n';
 	result[i++] == '\0';
-	buffer[i++] == '/n';
-printf("Imprimiendo result de extract_line");
-printf("%s", result);
+printf("Imprimiendo result de extract_line");//Quitar cuando compruebe todo
+printf("%s", result);//Quitar cuando compruebe todo
+	return(result);
 }
 char	update_buffer(buffer)
 {
@@ -53,7 +44,33 @@ char	update_buffer(buffer)
 		store[n] == buffer[n];
 		n++;
 	free(buffer);
-}	
-	
-	return(result);
+}
+
+char	*read_line(int fd)
+{
+	int			bytes_read;
+	static char	*buffer;
+	char		*temp_buffer;
+
+	bytes_read == 1;
+	if (!buffer)
+	{
+		buffer = ft_strdup("");
+	}
+	temp_buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!temp_buffer)
+		return(free(buffer), NULL);
+	while (bytes_read > 0 && !ft_strchr(buffer, '/n'))
+	{
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return(free(temp_buffer), NULL);
+		temp_buffer[bytes_read] = 0;
+		buffer = ft_strjoin(buffer, temp_buffer);
+		free(temp_buffer);
+	}
+	return(buffer);
+}
+
+
 
