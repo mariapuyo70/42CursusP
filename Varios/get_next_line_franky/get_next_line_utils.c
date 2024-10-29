9cdb8e8cb37d436ead6d52ef57c110ca459279d3
Maria <166420887+mariapuyo70@users.noreply.github.com>
2024-10-29 20:30:46 +0100

    Subida franky

---
+++ Varios/get_next_line_franky/get_next_line_utils.c
@@ -0,0 +1,203 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maria <mpuyo-ro@student.42malaga.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:27:44 by mpuyo-ro          #+#    #+#             */
+/*   Updated: 2024/10/29 18:51:00 by maria            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	i = 0;
+	while (src[i] != '\0' && dst_len + i < dstsize - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while ((i < dstsize - 1) && (src[i] != '\0'))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	len_s1, len_s2;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	result = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, len_s1 + 1);
+	ft_strlcat(result, s2, len_s1 + len_s2 + 1);
+	return (result);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*tmp;
+	size_t	size;
+
+	if (!s1)
+		return (NULL);
+	size = ft_strlen(s1) + 1;
+	tmp = (char *)malloc(size);
+	if (!tmp)
+		return (NULL);
+	ft_strlcpy(tmp, s1, size);
+	return (tmp);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (c == 0 ? (char *)s : NULL);
+}
+*/
+// get_next_line_utils.c
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	i = 0;
+	while (src[i] != '\0' && dst_len + i < dstsize - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while ((i < dstsize - 1) && (src[i] != '\0'))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	len_s1, len_s2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	result = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, len_s1 + 1);
+	ft_strlcat(result, s2, len_s1 + len_s2 + 1);
+	return (result);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*tmp;
+	size_t	size;
+
+	if (!s1)
+		return (NULL);
+	size = ft_strlen(s1) + 1;
+	tmp = (char *)malloc(size);
+	if (!tmp)
+		return (NULL);
+	ft_strlcpy(tmp, s1, size);
+	return (tmp);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (c == 0 ? (char *)s : NULL);
+}
